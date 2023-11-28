030a0bc0224072320f6e0ccc3c886a98858ad95d
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-11-28 13:44:08 +0200

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,50 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome home!");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+
+  abstract OpenDoor(Key: object): void;
+}
+
+class MyHouse extends House {
+  OpenDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
